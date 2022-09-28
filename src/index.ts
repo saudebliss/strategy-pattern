d78f47537a51d58d2336439a19a38b62438a6957
Erasmo De Souza Pinheiro <erasmopinheiro@Erasmos-MacBook-Air.local>
2022-09-28 13:57:17 -0300

    completed implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
-
+import { CalculatorContext } from "./calculadora/CalculatorContext";
+import { PlusStrategy } from "./calculadora/implementations/PlusStrategy";
+import { MultiplyStrategy } from "./calculadora/implementations/MultiplicacaoStrategy";
+import { DivideStrategy } from "./calculadora/implementations/DivideStrategy";
 const show = () => {
-  const a = 2;
-  const b = 1;
-  let result = 0;
+  // Plus
+  const calculador = new CalculatorContext(new PlusStrategy());
+  console.log(calculador.executeOperation(2, 3));
 
-  console.log(result);
+  // Minus
+  calculador.setStrategy(new PlusStrategy());
+  console.log(calculador.executeOperation(2, 3));
+
+  // Multiply
+  calculador.setStrategy(new MultiplyStrategy());
+  console.log(calculador.executeOperation(2, 3));
+
+  // Divide
+  calculador.setStrategy(new DivideStrategy());
+  console.log(calculador.executeOperation(2, 3));
 };
 show();
